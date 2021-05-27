b35751a9f4bfe09e272b616a67bbbdfd19a7f17f
floriansegginger <florian@fullrange.ch>
2021-05-27 17:29:00 +0200

    Add some comments

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,38 @@
 import './App.css';
 import React from 'react';
 
+// We need to grab these non-npm libraries from the global namespace
 let WoopsaClient = window.WoopsaClient;
 let jQuery = window.jQuery;
 
 class App extends React.Component {
+
   constructor(props) {
+    // React boilerplate code. We need to call the super constructor
     super(props);
 
+    // Initialize the state with a randomNumber = 0
     this.state = {
       randomNumber: 0.0
     };
 
+    // Create a woopsa client that connects to the server on localhost -- this is the C# server application
     this.woopsaClient = new WoopsaClient("http://localhost/woopsa", jQuery);
 
+    // We add an "onChange" listener. Which means, Woopsa will manage the fact that any time this
+    // value changes on the server, Woopsa will call our "randomNumberChanged" function 
     this.woopsaClient.onChange("/RandomNumber", this.randomNumberChanged.bind(this))
   }
 
+  // The random number has changed, so this function simply updates our state, which will trigger
+  // redrawing of the user interface (render function)
   randomNumberChanged(newValue) {
     this.setState({
       randomNumber: newValue
     });
   }
 
+  // The render function is called every time the state changes
   render() {
     return (
       <div className="App">
